d8ddafc20fb1a47291a74501be478dc571c4ee83
Neil Bowers <neil@bowers.com>
2013-08-21 20:53:32 +0100

    Added comments with thoughts / questions on how iterator should be done

--- lib/PAUSE/Packages.pm
+++ lib/PAUSE/Packages.pm
@@ -16,6 +16,22 @@ has '+basename' =>
      default => sub { return '02packages.txt'; },
     );
 
+#
+# I'm currently calling these as class methods:
+#
+#   my $iterator = PAUSE::Packages->release_iterator();
+#
+# But that results in the class being instantiated twice. I was trying to avoid:
+#
+#   my $packages = PAUSE::Packages->new();
+#   my $iterator = $packages->entry_iterator();
+#
+# I guess the first example could then be written:
+#
+#   my $iterator = PAUSE::Packages->new()->entry_iterator();
+#
+# ?
+
 sub entry_iterator
 {
     require PAUSE::Packages::EntryIterator;
